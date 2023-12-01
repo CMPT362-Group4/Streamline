bef44691a32e60cc98b3a006a7c7d217f7b8ed7f
Akinloluwa Ayomide Adedijo <89050852+Akinloluwa-Adedijo@users.noreply.github.com>
2023-12-01 15:39:54 -0800

    added tv shows

--- app/src/main/java/ca/sfu/cmpt362/group4/streamline/api_service/tmdbApiService.kt
+++ app/src/main/java/ca/sfu/cmpt362/group4/streamline/api_service/tmdbApiService.kt
@@ -1,6 +1,7 @@
 package ca.sfu.cmpt362.group4.streamline.api_service
 
 import ca.sfu.cmpt362.group4.streamline.data_models.MovieResponse
+import ca.sfu.cmpt362.group4.streamline.data_models.TvResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,4 +9,6 @@ import retrofit2.http.Query
 interface TmdbApiService {
     @GET("movie/popular")
     fun getPopularMovies(@Query("api_key") apiKey: String): Call<MovieResponse>
+    @GET("tv/popular")
+    fun getPopularTv(@Query("api_key") apiKey: String): Call<TvResponse>
 }

b54ff361674fe6ae9c0ffa622fee52dbb3eacbc6
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2023-12-05 12:48:04 -0800

    minor theme and visual changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
+    implementation("com.google.android.material:material:1.10.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
     testImplementation("junit:junit:4.13.2")
